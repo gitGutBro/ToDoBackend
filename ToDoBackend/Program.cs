b8e5cb90c214cdbe4c203f2c3abf24c7dc4f3aaf
gitGutBro <osero6110@gmail.com>
2025-08-29 20:26:45 +0300

    Integrate Serilog for structured logging
    Added Serilog for improved logging, including console and file sinks, and request logging middleware. Updated error handling to use Serilog and upgraded Swashbuckle.AspNetCore package version.

--- ToDoBackend/Program.cs
+++ ToDoBackend/Program.cs
@@ -1,7 +1,17 @@
+using Serilog;
 using ToDoBackend.Mappers;
 using ToDoBackend.Repositories;
 using ToDoBackend.Services;
 using ToDoBackend.Validators;
+
+const string LogFormat = "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} :: {Message:lj}{NewLine}{Exception}";
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(outputTemplate: LogFormat)
+    .WriteTo.File($"logs/logs-.txt", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .MinimumLevel.Debug()
+    .CreateLogger();
 
 try
 {
@@ -9,6 +19,7 @@ try
 
     // Add services to the container.
 
+    builder.Host.UseSerilog();
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
@@ -21,6 +32,8 @@ try
     builder.Services.AddSingleton<UpdateToDoMapper>();
 
     WebApplication app = builder.Build();
+
+    app.UseSerilogRequestLogging();
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
@@ -39,12 +52,12 @@ try
 }
 catch (Exception ex)
 {
-    Console.WriteLine($"An error occurred: {ex.Message}");
-    Environment.Exit(1);
+    Log.Fatal(ex, "Application start-up failed");
+    throw;
 }
 finally
 {
-    Console.WriteLine("Application has stopped.");
+    Log.CloseAndFlush();
 }
 
 public partial class Program;
